6ecbe8c6ad8aee3a602b6a5cffd5e6cd6af31689
Gharch <ali.carnoat@gmail.com>
2018-12-03 14:00:51 +0330

    redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 //
-import {Provider} from 'react-redux';
-import store from 'src/store/store';
-
-//const StoreInstance = store();
 
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
